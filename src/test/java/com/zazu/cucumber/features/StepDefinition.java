1761913ae58b0f6dbeb8b11a2796fee1866163c7
Cheehong Hsia <cheehong@zazu-it.com>
2016-01-03 00:00:47 +0100

    Extending feature using datatable

--- src/test/java/com/zazu/cucumber/features/StepDefinition.java
+++ src/test/java/com/zazu/cucumber/features/StepDefinition.java
@@ -1,10 +1,13 @@
 package com.zazu.cucumber.features;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
@@ -30,13 +33,29 @@ public class StepDefinition {
 		driver.navigate().to("http://thetestroom.com/webapp/");
 	}
 
-	@When("^I click on the About link$")
-	public void i_click_on_the_About_link() throws Throwable {
-		driver.findElement(By.id("about_link")).click();
+	@When("^I click on the \"([^\"]*)\" link$")
+	public void i_click_on_the_link(String link) throws Throwable {
+		driver.findElement(By.id(link)).click();
 	}
 
-	@Then("^I see the header displaying \"(.*?)\"$")
+	@Then("^I see the header displaying \"([^\"]*)\"$")
 	public void i_see_the_header_displaying(String header) throws Throwable {
 		Assert.assertEquals(header, driver.findElement(By.cssSelector("td.content > h1")).getText());
 	}
+
+	@When("^Fill in the form with valid data$")
+	public void fill_in_the_form_with_valid_data(DataTable table) throws Throwable {
+		List<List<String>> data = table.raw();
+		driver.findElement(By.name("name_field")).sendKeys(data.get(1).get(1));
+		driver.findElement(By.name("address_field")).sendKeys(data.get(2).get(1));
+		driver.findElement(By.name("postcode_field")).sendKeys(data.get(3).get(1));
+		driver.findElement(By.name("email_field")).sendKeys(data.get(4).get(1));
+		driver.findElement(By.id("submit_message")).click();
+	}
+
+	@Then("^I see a confirmationpage the text \"([^\"]*)\"$")
+	public void i_see_a_confirmationpage_the_text(String message) throws Throwable {
+		Assert.assertEquals(message, driver.findElement(By.cssSelector("td.content > h1")).getText());
+	}
+
 }
--- src/test/java/com/zazu/cucumber/features/myfeature.feature
+++ src/test/java/com/zazu/cucumber/features/myfeature.feature
@@ -2,5 +2,16 @@ Feature: My first Cucumber test
 
   Scenario: Test if the title is correct
     Given I am on the Zoo website
-    When I click on the About link
+    When I click on the "about_link" link
     Then I see the header displaying "ABOUT"
+
+  Scenario: Fill in the Contact Us form with valid info
+    Given I am on the Zoo website
+    When I click on the "contact_link" link
+    And Fill in the form with valid data
+      | Field    | Value                  |
+      | Name     | Cheehong               |
+      | Address  | Eerste Oosterparklaan  |
+      | Postcode | 3544AN                 |
+      | Email    | hsia.cheehong@gmailcom |
+    Then I see a confirmationpage the text "We have your message"
